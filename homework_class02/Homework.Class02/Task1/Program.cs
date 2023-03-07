3122de0ed85af2e796f62185cfe0c2cfb160bc0d
Mice Karajov <38554954+micekarajov@users.noreply.github.com>
2023-03-08 00:18:40 +0100

    Add files via upload

---
+++ homework_class02/Homework.Class02/Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.CodeAnalysis;
+Console.WriteLine("Task 1 - Real Calculator");
+
+Console.WriteLine("Please enter the first number: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Please enter the second number: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Please enter the operator ( +, - , * , / )");
+string operator1 = Console.ReadLine();
+
+if (operator1 == "+")
+{
+    int sum = number1 + number2;
+    Console.WriteLine($"The result is {sum}");
+} 
+else if ( operator1 == "-")
+{
+    int sub = number1 - number2;
+    Console.WriteLine($"The result is {sub}");
+} 
+else if ( operator1 == "*")
+{
+    int mult = number1 * number2;
+    Console.WriteLine($"The result is {mult}");
+} 
+else if ( operator1 == "/") 
+{
+    int div = number1 / number2;
+    Console.WriteLine($"The result is {div}");
+} else
+{
+    Console.WriteLine("Please enter the right operator!");
+}
