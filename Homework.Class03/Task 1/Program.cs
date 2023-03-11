7a68368b80e60ca1a490dd5b59bbb6bbe58c8eca
Mice Karajov <38554954+micekarajov@users.noreply.github.com>
2023-03-11 16:55:29 +0100

    Add files via upload

---
+++ Homework.Class03/Task 1/Program.cs
@@ -0,0 +1,32 @@
+﻿// ------- Homework Class 3 - Loops and Arrays-------
+Console.WriteLine("----- Homewokr Class 3 - Task 1");
+
+int[] numbersArray = new int[6];
+
+int sumOfEven = 0;
+
+for (int i = 0; i < numbersArray.Length; i++)
+{
+    Console.WriteLine($"Please enter random number no.{i + 1}");
+    bool isNumber = int.TryParse(Console.ReadLine(), out int randomNumber);
+
+    if (isNumber)
+    {
+        numbersArray[i] = randomNumber;
+    }
+    else
+    {
+        Console.WriteLine("Please enter a number!"); // koga ke vnesam pogresen input me nosi na slednoto iteriranje, kako da me vrati na istoto
+        // primer vnesi random broj 1: i vnesnuvame gresen input i naredno ne nosi na vnesi random broj 2...
+    }
+}
+
+foreach (int number in numbersArray)
+{
+    if (number % 2 == 0)
+    {
+        sumOfEven += number;
+    }
+}
+
+Console.WriteLine($"The sum of the even numbers from the array is {sumOfEven}");
---
+++ Homework.Class03/Task 2/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("----- Homewokr Class 3 - Task 2");
+
+string[] studentsG1 = new string[] { "Mitko Majorot", "Dimitar", "Eleonora", "Tode", "Nikola" };
+string[] studentsG2 = new string[] { "Zdravko", "Petko", "Janko", "Branko", "Trajko", "Stanko" };
+
+Console.WriteLine("Please enter a number(between 1 and 2): ");
+bool isNumber = int.TryParse(Console.ReadLine(), out int number);
+
+if (isNumber)
+{
+    if (number == 1)
+    {
+        Console.WriteLine("The students in G1 are:");
+        foreach (string students in studentsG1)
+        {
+            Console.WriteLine(students);
+        }
+    }
+    else if (number == 2)
+    {
+        Console.WriteLine("The students in G2 are:");
+        foreach (string students in studentsG2)
+        {
+            Console.WriteLine(students);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid input. Please enter a number between 1 and 2");
+    }
+}
+else
+{
+    Console.WriteLine("Please enter a number!");
+}
